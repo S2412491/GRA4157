f139e321fc52fea9872f06c21c83e2c0305fec69
Bohdan Hanhalo <bohdanho@Bohdanhos-MacBook-Air-15.local>
2024-09-27 13:37:03 +0200

    changed pipelines

--- scripts/fill_lists.py
+++ scripts/fill_lists.py
@@ -1,11 +1,11 @@
 from math import sqrt, exp, pi
 
-x_start = -4
-x_end = 4
-x_count = 40
-x_interval = (x_end - x_start) / x_count
+X_START = -4
+X_END = 4
+X_COUNT = 40
+X_INTERVAL = (X_END - X_START) / X_COUNT
 
-x_list = [ x_start + i * x_interval for i in range(x_count + 1) ]
+x_list = [ X_START + i * X_INTERVAL for i in range(X_COUNT + 1) ]
 h_list = [ 1 / sqrt(2 * pi) * exp(-1 / 2 * x ** 2) for x in x_list ]
 
 for x, h in zip(x_list, h_list):
